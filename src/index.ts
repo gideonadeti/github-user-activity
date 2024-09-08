e68c2a9ef6818b0c9f9d6d3ad5700415e0914ea5
gideonadeti <gideonadeti0@gmail.com>
2024-09-08 00:59:15 +0000

    Implement fetch data functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+import { GitHubEvent } from "./types";
+
 // Helper to log messages to the console
 function consoleLog(
   message: string,
-  type: "success" | "failure",
+  type: "success" | "failure" | "info",
   example?: string
 ) {
   const reset = "\x1b[0m";
@@ -12,29 +14,69 @@ function consoleLog(
   const colors = {
     success: "\x1b[32m", // Green
     failure: "\x1b[31m", // Red
+    info: "\x1b[34m", // Blue
   };
 
-  // Retrieve color based on the provided type
   const color = colors[type];
 
-  // Construct the message with or without example
   const fullMessage = example
     ? `${message}\nValid example: ${example}`
     : message;
 
-  // Log the message with color and reset
   console.log(`${color}${fullMessage}${reset}\n`);
 }
 
-const [, , ...args] = process.argv;
+// Fetch GitHub events for the user
+async function fetchGitHubEvents(
+  username: string
+): Promise<GitHubEvent[] | undefined> {
+  const url = `https://api.github.com/users/${username}/events?per_page=50`;
 
-if (args.length !== 1) {
-  const noArg = args.length === 0;
-  consoleLog(
-    noArg ? "Please provide a username." : "Please provide only one username.",
-    "failure", "gua gideonadeti"
-  );
-} else {
-  const username = args[0];
-  consoleLog(`Fetching data for ${username}...`, "success");
+  consoleLog("Fetching events...", "info");
+
+  try {
+    const response = await fetch(url); // Ensure fetch is available
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const data = await response.json();
+    console.clear();
+    return data;
+  } catch (error) {
+    console.log("\x1b[31mFailed to fetch events.\x1b[0m");
+  }
 }
+
+// Main function to handle user input and fetch events
+async function main() {
+  const [, , ...args] = process.argv;
+
+  if (args.length !== 1) {
+    const noArg = args.length === 0;
+    consoleLog(
+      noArg
+        ? "Please provide a username."
+        : "Please provide only one username.",
+      "failure",
+      "gua gideonadeti"
+    );
+  } else {
+    const username = args[0];
+    const events = await fetchGitHubEvents(username);
+
+    if (events && events.length > 0) {
+      consoleLog(
+        `Showing ${events.length} recent events from ${username}...`,
+        "success"
+      );
+    } else {
+      events?.length === 0 &&
+        consoleLog(`No events found for ${username}.`, "success");
+    }
+  }
+}
+
+// Run the main function
+main();
